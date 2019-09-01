58ea10607b2d87f109a8c27bcc7144ea673f060f
LandrumTrev <landrum.trevillian@gmail.com>
2019-09-01 01:42:31 -0400

    misc code nonsense

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,4 +1,6 @@
 // SAMPLE CLASS CONSTRUCTION OF A SOURCE CODE .java FILE
+// COMPILE: $ javac MyFirstApp.java
+// RUN: $ java MyFirstApp
 
 // public means everyone can access it
 public class MyFirstApp {
@@ -11,6 +13,36 @@ public class MyFirstApp {
     // print this out to the CLI
     System.out.println("I Rule!");
     System.out.println("The World.");
+
+    // declarations, assignments, method calls, etc
+    int x = 3;
+    String name = "Dirk";
+    x = x * 17;
+    System.out.print("x is " + x + "\n");
+    // double d = Math.random();
+
+    // while loop
+    while (x > 12) {
+      x = x - 1;
+    }
+
+    // for loop
+    for (int y = 0; y < 10; y = y + 1) {
+      System.out.print("y is now " + y + "\n");
+    }
+
+    // if/else conditional tests
+    if (x == 10) {
+      System.out.print("x must be 10\n");
+    } else {
+      System.out.print("x isn't 10\n");
+    }
+
+    if ((x < 3) & (name.equals("Dirk"))) {
+      System.out.print(name + "Gently\n");
+    }
+
+    System.out.print("this line runs no matter what\n");
 
   }
 

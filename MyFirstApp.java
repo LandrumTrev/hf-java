76e83247cb453050914e2a55436bb1b300903a79
LandrumTrev <landrum.trevillian@gmail.com>
2019-09-01 01:09:34 -0400

    MyFirstApp compiled and run

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -3,13 +3,14 @@
 // public means everyone can access it
 public class MyFirstApp {
 
-  // void means no return value
-  // main is this method's name...the class's "main" method?
+  // "void" means method does not return a value
+  // "main" is this method's name...the class's "main" method
   // method expects an array of Strings and will call it "args"
   public static void main(String[] args) {
 
     // print this out to the CLI
-    System.out.print("I Rule!");
+    System.out.println("I Rule!");
+    System.out.println("The World.");
 
   }
 

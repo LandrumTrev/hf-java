68dbfe3d89baf128f635bde2295cb6396d9ea418
LandrumTrev <landrum.trevillian@gmail.com>
2019-09-01 01:01:42 -0400

    first commit

---
+++ sampleSource.java
@@ -0,0 +1,16 @@
+// SAMPLE CLASS CONSTRUCTION OF A SOURCE CODE .java FILE
+
+// public means everyone can access it
+public class MyFirstApp {
+
+  // void means no return value
+  // main is this method's name...the class's "main" method?
+  // method expects an array of Strings and will call it "args"
+  public static void main(String[] args) {
+
+    // print this out to the CLI
+    System.out.print("I Rule!");
+
+  }
+
+}

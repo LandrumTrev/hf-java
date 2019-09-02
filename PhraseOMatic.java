2b8e3c43affb506c8221d19c9a56e00fea6a5514
LandrumTrev <landrum.trevillian@gmail.com>
2019-09-02 00:01:56 -0400

    comment cleanup

--- PhraseOMatic.java
+++ PhraseOMatic.java
@@ -14,17 +14,19 @@ public class PhraseOMatic {
     String[] wordListThree = { "process", "tipping point", "solution", "architecture", "core competency", "strategy",
         "mindshare", "portal", "space", "vision", "paradigm", "mission" };
 
-    // get a count of the number of words in each array
+    // get a count of the number of words in each array using its .length property
     int oneLength = wordListOne.length;
     int twoLength = wordListTwo.length;
     int threeLength = wordListThree.length;
 
-    // generate random numbers based on the length of each array
+    // generate random numbers based on the length of each array:
+    // Math.random() returns a number between 0 and 1, multiplied by array's length,
+    // (int) "casts" a whole number (integer) of the decimal (float) generated
     int rand1 = (int) (Math.random() * oneLength);
     int rand2 = (int) (Math.random() * twoLength);
     int rand3 = (int) (Math.random() * threeLength);
 
-    // build corporate gibberish phrase with random word from each array
+    // build phrase by concatenating a random element from each array
     String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
 
     // print out a sentence using the generated phrase to the CLI

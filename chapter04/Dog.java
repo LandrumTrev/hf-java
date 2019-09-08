72c36a56b42d881eae9fc3dfe9cd7854fb15b36f
LandrumTrev <landrum.trevillian@gmail.com>
2019-09-08 14:20:52 -0400

    Dog, intro to method and inst var relationships

---
+++ chapter04/Dog.java
@@ -0,0 +1,20 @@
+// intro to relationships between methods and instance variables
+
+class Dog {
+
+  int size;
+  String name;
+
+  void bark() {
+
+    if (size > 60) {
+      System.out.println("Wooof! Wooof!");
+    } else if (size > 14) {
+      System.out.println("Ruff! Ruff!");
+    } else {
+      System.out.println("Yip! Yip!");
+    }
+    
+  }
+
+}
